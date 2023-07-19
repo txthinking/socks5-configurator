1156e9b56e745bdbeb1bdfb8a9f4377c45f3cc85
txthinking <cloud@txthinking.com>
2023-07-19 17:35:11 +0800

    edge

--- options.js
+++ options.js
@@ -30,17 +30,9 @@ chrome.storage.local.get('bypassswitch', s =>{
         document.querySelector('#bypassswitch').checked = false;
     }
 });
-chrome.storage.local.get('bypassdomainurl', s =>{
-    s = s.bypassdomainurl || 'https://txthinking.github.io/bypass/china_domain.txt';
-    document.querySelector('#bypassdomainurl').value = s;
-});
-chrome.storage.local.get('bypasscidr4url', s =>{
-    s = s.bypasscidr4url || 'https://txthinking.github.io/bypass/china_cidr4.txt';
-    document.querySelector('#bypasscidr4url').value = s;
-});
-chrome.storage.local.get('bypasscidr6url', s =>{
-    s = s.bypasscidr6url || 'https://txthinking.github.io/bypass/china_cidr6.txt';
-    document.querySelector('#bypasscidr6url').value = s;
+chrome.storage.local.get('bypassdomain', s =>{
+    s = s.bypassdomain || "cn\napple.com";
+    document.querySelector('#bypassdomain').value = s;
 });
 
 document.querySelector('#save').addEventListener("click", async (e) => {
@@ -50,9 +42,7 @@ document.querySelector('#save').addEventListener("click", async (e) => {
     var socks5switch = document.querySelector('#socks5switch').checked;
     var socks5server = document.querySelector('#socks5server').value;
     var bypassswitch = document.querySelector('#bypassswitch').checked;
-    var bypassdomainurl = document.querySelector('#bypassdomainurl').value;
-    var bypasscidr4url = document.querySelector('#bypasscidr4url').value;
-    var bypasscidr6url = document.querySelector('#bypasscidr6url').value;
+    var bypassdomain = document.querySelector('#bypassdomain').value;
 
     if(socks5switch){
         if(!/.+:\d+/.test(socks5server)){
@@ -71,51 +61,20 @@ document.querySelector('#save').addEventListener("click", async (e) => {
 		"172.16.0.0/12",
 		"192.168.0.0/16",
 		"224.0.0.0/4",
+		"::/127",
         "<local>",
         "<localhost>",
         "*.local",
 	];
     if(bypassswitch){
-        try{
-            var r = await fetch(bypassdomainurl);
-            if(r.status != 200){
-                throw Error(`When fetch bypass list: ${r.status}`);
-            }
-            var s = await r.text();
-            var l1 = s.trim().split('\n');
-            l1.forEach(v=>{
-                l.push(v.trim());
-                l.push("*."+v.trim());
-            });
-            var r = await fetch(bypasscidr4url);
-            if(r.status != 200){
-                throw Error(`When fetch bypass list: ${r.status}`);
-            }
-            var s = await r.text();
-            var l1 = s.trim().split('\n');
-            l1.forEach(v=>{
-                l.push(v.trim());
-            });
-            var r = await fetch(bypasscidr6url);
-            if(r.status != 200){
-                throw Error(`When fetch bypass list: ${r.status}`);
-            }
-            var s = await r.text();
-            var l1 = s.trim().split('\n');
-            l1.forEach(v=>{
-                l.push(v.trim());
-            });
-        }catch(e){
-            alert(`When fetch bypass list: ${e.message}`);
-            document.querySelector('#save').style.display = 'block';
-            document.querySelector('#ing').style.display = 'none';
-            return;
-        }
+        var l1 = bypassdomain.trim().split('\n');
+        l1.forEach(v=>{
+            l.push(v.trim());
+            l.push("*."+v.trim());
+        });
     }
     chrome.storage.local.set({"bypassswitch": bypassswitch ? 'on' : 'off'});
-    chrome.storage.local.set({"bypassdomainurl": bypassdomainurl});
-    chrome.storage.local.set({"bypasscidr4url": bypasscidr4url});
-    chrome.storage.local.set({"bypasscidr6url": bypasscidr6url});
+    chrome.storage.local.set({"bypassdomain": bypassdomain});
 
     if(!socks5switch){
         chrome.proxy.settings.set({
--- readme.md
+++ readme.md
@@ -8,6 +8,8 @@ Configure your Chrome with socks5 proxy
 
 [Chrome Web Store](https://chrome.google.com/webstore/detail/hnpgnjkeaobghpjjhaiemlahikgmnghb)
 
+[Edge Web Store](https://microsoftedge.microsoft.com/addons/detail/makcfnkaapoandbeldfnmamikefoafao)
+
 ## License
 
 Licensed under The MIT License
